6eccd924cfec3844ee331f155b0814d884be1bc2
e-tony <20404466+e-tony@users.noreply.github.com>
2020-10-07 14:12:34 +0200

    update

--- setup.sh
+++ setup.sh
@@ -6,6 +6,3 @@ enableCORS=false\n
 port = $PORT\n
 \n
 " > ~/.streamlit/config.toml
-mkdir -p ~/models/
-gdown https://drive.google.com/uc?id=1GG1xCzZpoys-cRMyKCtwDaqyY2WQaCCl -O ~/models/cc.en.25.bin.gz
-gunzip ~/models/cc.en.25.bin.gz ~/models/
