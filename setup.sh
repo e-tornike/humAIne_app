8bdbb30e49aff53d48a122c0473bc1ff2248613e
e-tony <20404466+e-tony@users.noreply.github.com>
2020-10-07 14:04:17 +0200

    update

--- setup.sh
+++ setup.sh
@@ -7,4 +7,5 @@ port = $PORT\n
 \n
 " > ~/.streamlit/config.toml
 mkdir -p ~/models/
-gdown https://drive.google.com/uc?id=1GG1xCzZpoys-cRMyKCtwDaqyY2WQaCCl ~/models/
+gdown https://drive.google.com/uc?id=1GG1xCzZpoys-cRMyKCtwDaqyY2WQaCCl -O ~/models/cc.en.25.bin.gz
+gunzip ~/models/cc.en.25.bin.gz ~/models/
