5615455979c5d0da2f7f25d3c7438b06cac8f0f2
e-tony <20404466+e-tony@users.noreply.github.com>
2020-10-07 15:19:56 +0200

    update

--- setup.sh
+++ setup.sh
@@ -8,4 +8,3 @@ port = $PORT\n
 " > ~/.streamlit/config.toml
 mkdir -p ~/models/
 wget ./models/cc.en.25.bin.gz https://www.dropbox.com/s/06zx30696rdllg2/cc.en.25.bin.gz?dl=0
-gunzip ./models/cc.en.25.bin.gz ./models/
