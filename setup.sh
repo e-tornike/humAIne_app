41cba2befe1ed7448727373a402744b10bf09955
e-tony <20404466+e-tony@users.noreply.github.com>
2020-10-07 14:52:24 +0200

    update

--- setup.sh
+++ setup.sh
@@ -7,6 +7,5 @@ port = $PORT\n
 \n
 " > ~/.streamlit/config.toml
 mkdir -p ~/models/
-aria2c -x16 https://drive.google.com/uc?id=1GG1xCzZpoys-cRMyKCtwDaqyY2WQaCCl
-mv ~/cc.en.25.bin.gz ~/models/
+aria2c -x16 https://drive.google.com/uc?id=1GG1xCzZpoys-cRMyKCtwDaqyY2WQaCCl -d ~/models
 gunzip ~/models/cc.en.25.bin.gz ~/models/
